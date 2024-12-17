79e68c6605e9b849b8e6c801833981352afd5c08
h2lh-dev <hugo.desmons59@gmail.com>
2024-12-17 10:35:14 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import svelte from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-	plugins: [svelte()],
-	css: {
-		postcss: {
-			plugins: [require('autoprefixer')],
-		},
-	},
+	plugins: [sveltekit()]
 });
