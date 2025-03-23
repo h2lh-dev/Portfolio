dc5b5e157e2ee9b6963db5053f29c46b18dd09bb
h2lh-dev <hugo.desmons59@gmail.com>
2025-03-23 21:53:54 +0100

    mise a jour portfolio

--- src/routes/Projet/+page.svelte
+++ src/routes/Projet/+page.svelte
@@ -7,10 +7,20 @@
 <body>
 <div class="portfolio">
     <div class="project">
+        <img src="./Projet/ChatMoiFort.png" alt="Projet 2">
+        <div class="project-info">
+            <h2>ChatMoiFort</h2>
+            <p>Dans le cadre de ce projet, nous avons dû réaliser une application de messagerie en ligne. En utilisant TomCat.</p>
+            <a href="https://gitlab.univ-lille.fr/matias.mennecart.etu/sae-r4.01"> Git </a>
+            <img src="https://skillicons.dev/icons?i=java,bootstrap,css,html,postgresql&theme=dark&perline=15" alt="My Skills" />
+        </div>
+    </div>
+    <div class="project">
         <img src="./Projet/Classification.png" alt="Projet 2">
         <div class="project-info">
             <h2>Logiciel de Classifcation</h2>
             <p>Dans le cadre de ce projet, nous avons dû réaliser une application de classification permettant de charger un jeu de données et de le classifier en utilisant l'algorithme k-NN.</p>
+            <a href="https://gitlab.univ-lille.fr/sae302/2024/H4_SAE3.3"> Git </a>
             <img src="https://skillicons.dev/icons?i=java,git,figma&theme=dark&perline=15" alt="My Skills" />
         </div>
     </div>
@@ -19,6 +29,7 @@
         <div class="project-info">
             <h2>RentApple</h2>
             <p>Dans le cadre de ce projet, nous avons dû réaliser le site web fictif d'une entreprise en répondant à un besoin de création d'une plateforme de covoiturage.</p>
+            <a href="https://github.com/h2lh-dev/RentApple"> Git </a>
            <img src="https://skillicons.dev/icons?i=html,css&theme=dark&perline=15" alt="My Skills" />
         </div>
     </div>
@@ -27,6 +38,8 @@
         <div class="project-info">
             <h2>Nuit de l'info</h2>
             <p>La Nuit de l'Info est un concours national qui rassemble les étudiants en informatique pour la création en équipe d'un site web proposant une solution numérique à des problématiques réelles.</p>
+            <a href="https://github.com/h2lh-dev/HDFC"> Git </a>
+            <a href="hdfc-beige.vercel.app"> Lien </a>
             <img src="https://skillicons.dev/icons?i=html,css,javascript,svelte,git&theme=dark&perline=15" alt="My Skills" />
         </div>
     </div>
@@ -35,6 +48,7 @@
         <div class="project-info">
             <h2>PirAttack</h2>
             <p>Nous avons dû réaliser un projet en 5 jours portant sur la création d'un jeu simple en Java, en utilisant la méthodologie Agile.</p>
+            <a href="https://gitlab.univ-lille.fr/2024-projet-agile-de-rentree/groupe-7"> Git </a>
             <img src="https://skillicons.dev/icons?i=java,git&theme=dark&perline=15" alt="My Skills" />
         </div>
     </div>
@@ -43,6 +57,7 @@
         <div class="project-info">
             <h2>Sam</h2>
             <p>Nous avons dû créer une application permettant de choisir le meilleur itinéraire en répondant aux critères de la personne (temps, prix et écologie). Nous avons utilisé l'algorithme de Dijkstra.</p>
+            <a href="https://gitlab.univ-lille.fr/sae2.01-2.02/2024/B1"> Git </a>
             <img src="https://skillicons.dev/icons?i=java,git&theme=dark&perline=15" alt="My Skills" />
         </div>
     </div>
@@ -61,6 +76,7 @@
         <div class="project-info">
             <h2>Base de donnée Parcoursup</h2>
             <p>Nous avons dû ventiler un jeu de données représentant les résultats de Parcoursup sur PostgreSQL et répondre à des questions statistiques à l'aide de requêtes SQL.</p>
+
 
             <img src="https://skillicons.dev/icons?i=postgresql&theme=light&perline=15" alt="My Skills" />
 
